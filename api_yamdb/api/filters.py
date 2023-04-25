c0a07a821356ee3393b54cd582f44707085184da
Denis-Loginov <114280260+Denis-Loginov@users.noreply.github.com>
2023-04-25 12:22:31 +0400

    Update filters.py

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -7,7 +7,6 @@ class TitleFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr="contains")
     category = filters.CharFilter(field_name="category__slug")
     genre = filters.CharFilter(field_name="genre__slug")
-    year = filters.NumberFilter(field_name="year")
 
     class Meta:
         model = Title
